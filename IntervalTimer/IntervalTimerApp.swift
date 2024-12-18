aadf3e392bb62a7181d2b9b2d35d996b91f707a5
wolfayet0855 <online_alt02@yahoo.com>
2024-12-17 20:34:02 -0500

    Initial Commit

---
+++ IntervalTimer/IntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalTimerApp.swift
+//  IntervalTimer
+//
+//  Created by user on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct IntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
