afd35487aa21c61e8f4c4a980708d4438faa68ea
wolfayet0855 <online_alt02@yahoo.com>
2024-12-19 14:52:28 -0500

    sound integration

--- IntervalTimer/ContentView.swift
+++ IntervalTimer/ContentView.swift
@@ -6,6 +6,7 @@
 //
 import SwiftUI
 import Combine
+import AVFoundation
 
 @main
 struct IntervalTimerApp: App {
@@ -25,40 +26,49 @@ struct ContentView: View {
     @State private var sets: Int = 1
     @State private var currentSet: Int = 1
     @State private var isResting: Bool = false
-
+    @State private var activityComplete: Bool = false
+    @State private var audioPlayer: AVAudioPlayer?
+    
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 20) {
                 Text("Interval Timer")
-                    .font(.largeTitle)
-                    .bold()
+                    .font(.system(size: 50, weight: .bold)) // Increased font size for title
                     .padding(.top, 50)
                     .padding()
                     .foregroundColor(Color.black)
-
+                
                 ZStack {
                     Circle()
                         .stroke(Color.gray.opacity(0.2), lineWidth: 20)
                         .frame(width: 200, height: 200)
-
+                    
                     Circle()
                         .trim(from: 0, to: progress())
                         .stroke(isResting ? Color.cyan : Color.green, style: StrokeStyle(lineWidth: 20, lineCap: .round))
                         .frame(width: 200, height: 200)
                         .rotationEffect(.degrees(-90))
                         .animation(.linear(duration: 1), value: progress())
-
-                    Text(formatTime(seconds: currentTime))
-                        .font(.system(size: 48, weight: .bold, design: .monospaced))
-                        .foregroundColor(Color.primary)
+                    
+                    if activityComplete {
+                        Text("Mission Complete")
+                            .font(.system(size: 48, weight: .bold, design: .rounded))
+                            .foregroundColor(Color.green)
+                            .multilineTextAlignment(.center)
+                    } else {
+                        Text(formatTime(seconds: currentTime))
+                            .font(.system(size: 48, weight: .bold, design: .monospaced))
+                            .foregroundColor(Color.primary)
+                    }
                 }
-
-                Text(isResting ? "Rest" : "Set \(currentSet) of \(sets)")
+                
+                Text(activityComplete ? "Great Work!" : (isResting ? "Rest" : "Set \(currentSet) of \(sets)"))
                     .font(.title2)
                     .foregroundColor(Color.primary)
-
+                
                 HStack(spacing: 20) {
-                    Button(action: startTimer) {
+                    Button(action: { startTimer() }) {
                         Text(isRunning ? "Pause" : "Start")
                             .font(.title)
                             .padding()
@@ -66,8 +76,8 @@ struct ContentView: View {
                             .foregroundColor(.white)
                             .cornerRadius(12)
                     }
-
-                    Button(action: resetTimer) {
+                    
+                    Button(action: { resetTimer() }) {
                         Text("Reset")
                             .font(.title)
                             .padding()
@@ -76,23 +86,24 @@ struct ContentView: View {
                             .cornerRadius(12)
                     }
                 }
-
+                
                 Spacer()
-
+                
                 NavigationLink(destination: SettingsView(timerDuration: $timerDuration, restDuration: $restDuration, sets: $sets)) {
                     Text("Interval Configuration")
                         .font(.title2)
                         .padding()
-                        .background(Color.blue.opacity(0.8))
+                        .background(Color.blue)
                         .foregroundColor(.white)
                         .cornerRadius(12)
-                        .padding(.bottom, 20) // Add bottom padding to the button
+                        .padding(.bottom, 20)
                 }
             }
             .padding()
+            .onRotate(perform: handleOrientationChange)
         }
     }
-
+    
     private func startTimer() {
         if isRunning {
             timer?.invalidate()
@@ -100,32 +111,39 @@ struct ContentView: View {
             if currentTime == 0 {
                 if isResting {
                     if currentSet < sets {
+                        playSound(soundName: "work")
                         currentSet += 1
                         isResting = false
                         currentTime = timerDuration
                     } else {
-                        timer?.invalidate()
+                        playSound(soundName: "complete")
+                        completeActivity()
                         return
                     }
                 } else {
+                    playSound(soundName: "rest")
                     isResting = true
                     currentTime = restDuration
                 }
             }
-
+            
             timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
                 if currentTime > 0 {
                     currentTime -= 1
                 } else {
                     if isResting {
                         if currentSet < sets {
+                            playSound(soundName: "work")
                             currentSet += 1
                             isResting = false
                             currentTime = timerDuration
                         } else {
-                            timer?.invalidate()
+                            playSound(soundName: "complete")
+                            completeActivity()
+                            return
                         }
                     } else {
+                        playSound(soundName: "rest")
                         isResting = true
                         currentTime = restDuration
                     }
@@ -134,24 +152,52 @@ struct ContentView: View {
         }
         isRunning.toggle()
     }
-
+    
     private func resetTimer() {
         timer?.invalidate()
         currentTime = timerDuration
         currentSet = 1
         isResting = false
         isRunning = false
+        activityComplete = false
     }
-
+    
+    private func completeActivity() {
+        timer?.invalidate()
+        isRunning = false
+        activityComplete = true
+    }
+    
     private func formatTime(seconds: Int) -> String {
         let minutes = seconds / 60
         let seconds = seconds % 60
         return String(format: "%02d:%02d", minutes, seconds)
     }
-
+    
     private func progress() -> CGFloat {
         let totalTime = isResting ? restDuration : timerDuration
         return CGFloat(currentTime) / CGFloat(totalTime)
+    }
+    
+    private func handleOrientationChange(orientation: UIDeviceOrientation) {
+        if orientation.isLandscape {
+            print("Switched to landscape mode")
+        } else if orientation.isPortrait {
+            print("Switched to portrait mode")
+        }
+    }
+    
+    private func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("Sound asset \(soundName) not found in Asset Catalog.")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer?.play()
+        } catch {
+            print("Error creating audioPlayer: \(error.localizedDescription)")
+        }
     }
 }
 
@@ -166,13 +212,13 @@ struct SettingsView: View {
                 .font(.largeTitle)
                 .padding()
 
-            Stepper(value: $timerDuration, in: 10...3600, step: 10) {
+            Stepper(value: $timerDuration, in: 10...3600, step: 5) {
                 Text("Work Duration: \(timerDuration) seconds")
                     .font(.title2)
             }
             .padding()
 
-            Stepper(value: $restDuration, in: 10...600, step: 10) {
+            Stepper(value: $restDuration, in: 10...600, step: 5) {
                 Text("Rest Duration: \(restDuration) seconds")
                     .font(.title2)
             }
@@ -189,3 +235,11 @@ struct SettingsView: View {
         .padding()
     }
 }
+
+    extension View {
+        func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+            self.onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+        }
+    }
