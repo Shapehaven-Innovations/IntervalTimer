08474e5878c93257c9e49bcc2c4cc208e0568d5b
wolfayet0855 <online_alt02@yahoo.com>
2025-05-26 16:58:05 -0400

    20252605- Reduce background options to light and dark

--- IntervalTimer/Configuration/BackgroundOption.swift
+++ IntervalTimer/Configuration/BackgroundOption.swift
@@ -6,21 +6,12 @@
 //
 
 
-//
-//  BackgroundOption.swift
-//  IntervalTimer
-//
-//  Created by You on 5/25/25.
-//
-
 import SwiftUI
 
 /// The four backgrounds your user can choose.
 enum BackgroundOption: String, CaseIterable, Identifiable {
-    case black  = "Black"
-    case white  = "White"
-    case orange = "Orange"
-    case blue   = "Blue"
+    case black  = "Dark"
+    case white  = "Light"
 
     var id: String { rawValue }
 
@@ -29,10 +20,6 @@ enum BackgroundOption: String, CaseIterable, Identifiable {
         switch self {
         case .black:  return .black
         case .white:  return .white
-        case .orange: return .orange
-        case .blue:
-            // “Light lumination” blue
-            return Color.blue.opacity(0.2)
         }
     }
 }
