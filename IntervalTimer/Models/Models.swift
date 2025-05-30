83ea96a3570cca66ed0ae5e5386fedb050c3b1c1
wolfayet0855 <online_alt02@yahoo.com>
2025-05-30 12:37:29 -0400

    20253005- update intentions and models

---
+++ IntervalTimer/Models/Models.swift
@@ -0,0 +1,27 @@
+//
+//  IntentRecord.swift
+//  IntervalTimer
+//
+//  Created by user on 5/30/25.
+//
+
+
+//
+//  Models.swift
+//  IntervalTimer
+//
+
+import Foundation
+
+/// A record of one workout‐intention (state of mind) entry.
+public struct IntentRecord: Identifiable, Codable {
+    public let id: UUID
+    public let date: Date
+    public let state: String
+
+    public init(date: Date, state: String) {
+        self.id    = UUID()
+        self.date  = date
+        self.state = state
+    }
+}
